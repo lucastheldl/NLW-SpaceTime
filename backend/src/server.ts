b91ab8c05068d01fb4f2782eaa91349121a81b59
lucastheldl <lucastheldl@gmail.com>
2023-05-17 19:55:29 -0300

    feat: add cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,13 @@
 import fastify from "fastify";
+import cors from "@fastify/cors";
 import { memoriesRoutes } from "./routes/memories";
 
 const app = fastify();
+
+app.register(cors, {
+  origin: true, //todas url de front end pode acessa o backend
+  //origin: ["endereço de desenvolv","endereço de produção"],
+});
 app.register(memoriesRoutes);
 
 app
