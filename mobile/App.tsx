1b5c179f29027854d1adc1a9fe000c6b56687e79
lucastheldl <lucastheldl@gmail.com>
2023-05-17 14:50:26 -0300

    feat: completed the mobile homepage

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  ImageBackground,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { BaiJamjuree_700Bold } from "@expo-google-fonts/bai-jamjuree";
+
+import blur from "./src/assets/bg-blur.png";
+import Stripes from "./src/assets/stripes.svg";
+import Logo from "./src/assets/logo.svg";
+
+import { styled } from "nativewind";
+
+const StyleStripes = styled(Stripes);
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    //melhor criar um component abaixo
+    return null;
+  }
+
   return (
-    <View className="bg-gray-950 flex-1 items-center justify-center">
-      <Text className='text-zinc-100 font-bold text-5xl'>Open up App.js to start working on your app!</Text>
-      <StatusBar style="light"/>
-    </View>
+    <ImageBackground
+      source={blur}
+      imageStyle={{ position: "absolute", left: "-100%" }}
+      className="bg-gray-900 px-8 flex-1 items-center relative py-10"
+    >
+      <StyleStripes className="absolute left-2" />
+      <View className="flex-1 items-center justify-center gap-6">
+        <Logo />
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Sua cápsula do tempo
+          </Text>
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+            Colecione momentos marcantes da sua jornada e compartilhe (se
+            quiser) com o mundo!
+          </Text>
+        </View>
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="rounded-full bg-green-500 px-5 py-2"
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            COMEÇAR A CADASTRAR
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <Text className="text-center font-body text-gray-300 text-sm leading-relaxed text-gray-200">
+        Feito com amor no NLW.
+      </Text>
+      <StatusBar style="light" />
+    </ImageBackground>
   );
 }
-
